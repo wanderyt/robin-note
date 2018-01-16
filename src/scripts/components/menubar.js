61009ba8fa1a9ad42410b32b48078cd187529ff6
wanderyt <wanderyt1988@163.com>
2018-01-16 14:09:30 +0800

    Introduce redux.

---
+++ src/scripts/components/menubar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import * as MenuConfig from '../../config/menu-info.json';
+
+const MenuBar = (props) => {
+    getMenus() {
+        let menus = [];
+
+    }
+    return (
+        <div
+            className={`MenuBar ${props.classNames}`}>
+
+        </div>
+    )
+}
