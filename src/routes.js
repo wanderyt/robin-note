04ab67ae0fac91cc1b00e06abce5e7ea96cd8a75
wanderyt <wanderyt1988@163.com>
2019-01-12 10:58:10 +0800

    Add code split according to routes.

--- src/routes.js
+++ src/routes.js
@@ -1,27 +1,31 @@
 // ./src/routes.js
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Number from './scripts/page/number';
-import Paint from './scripts/page/paint';
-import Instagram from './scripts/page/instagram';
-import Chinese from './scripts/page/chinese';
-import Default from './scripts/page/default';
-import Finance from './scripts/page/finance';
-import FacebookLogin from './scripts/page/facebook-login';
-import TestCounter from './scripts/page/test-counter';
-import Colors from './scripts/page/colors';
+
+const Number = lazy(() => import(/* webpackChunkName: "page-number" */ './scripts/page/number'));
+const Paint = lazy(() => import(/* webpackChunkName: "page-paint" */ './scripts/page/paint'));
+const Instagram = lazy(() => import(/* webpackChunkName: "page-instagram" */ './scripts/page/instagram'));
+const Chinese = lazy(() => import(/* webpackChunkName: "page-chinese" */ './scripts/page/chinese'));
+const Default = lazy(() => import(/* webpackChunkName: "page-default" */ './scripts/page/default'));
+const Finance = lazy(() => import(/* webpackChunkName: "page-finance" */ './scripts/page/finance'));
+const FacebookLogin = lazy(() => import(/* webpackChunkName: "page-facebook-login" */ './scripts/page/facebook-login'));
+const TestCounter = lazy(() => import(/* webpackChunkName: "page-facebook-login" */ './scripts/page/test-counter'));
+const Colors = lazy(() => import(/* webpackChunkName: "page-facebook-login" */ './scripts/page/colors'));
 
 export default (props) => (
   <div>
-    {/* React routers settings */}
-    <Route exact={true} path="/" component={Default}></Route>
-    <Route path="/number" component={Number}></Route>
-    <Route path="/paint" component={Paint}></Route>
-    <Route path="/instagram" component={Instagram}></Route>
-    <Route path="/chinese" component={Chinese}></Route>
-    <Route path="/finance" component={Finance}></Route>
-    <Route path="/facebookLogin" component={FacebookLogin}></Route>
-    <Route path="/testCounter" component={TestCounter}></Route>
-    <Route path="/colors" component={Colors}></Route>
+    <Suspense
+      fallback={<div>Spinner</div>}>
+      {/* React routers settings */}
+      <Route exact={true} path="/" component={Default}></Route>
+      <Route path="/number" component={Number}></Route>
+      <Route path="/paint" component={Paint}></Route>
+      <Route path="/instagram" component={Instagram}></Route>
+      <Route path="/chinese" component={Chinese}></Route>
+      <Route path="/finance" component={Finance}></Route>
+      <Route path="/facebookLogin" component={FacebookLogin}></Route>
+      <Route path="/testCounter" component={TestCounter}></Route>
+      <Route path="/colors" component={Colors}></Route>
+    </Suspense>
   </div>
 );
